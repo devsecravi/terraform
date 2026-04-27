da6bf389a7b0f57215a56a6f7646ca0a4ad15467
unknown <dasariravid145@gmail.com>
2026-04-27 20:06:01 +0530

    action

---
+++ dynamic/variable.tf
@@ -0,0 +1,20 @@
+variable "ingress_rules" {
+
+      default = [
+          {
+             port = 22
+             cidr_blocks = "tcp"
+             description = "allowd form 22 port"
+          }
+          {
+             port = 3303
+             cidr_blocks = "tcp"
+             description = "allowd form 3303 port"
+          }
+          {
+             port = 443
+             cidr_blocks = "tcp"
+             description = "allowd form 443 port"
+          }
+      ]
+}
