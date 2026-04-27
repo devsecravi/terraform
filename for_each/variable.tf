57a5c72412dd450248b439d7da5bc3bac77d8f20
unknown <dasariravid145@gmail.com>
2026-04-27 19:54:22 +0530

    action

--- for_each/variable.tf
+++ for_each/variable.tf
@@ -1,12 +1,29 @@
+  #  variable "instances"{
+
+  #      type = map(string)
+   #     default = {
+#
+  #          mongodb = "t3.micro"
+  #          redis = "t3.micro"
+  #          rabbitmq = "t3.micro"
+  #          catalogue = "t3.small"
+   #         payment = "t3.small"
+   #     }
+   # }
 variable "instances"{
 
-    type = map(string)
-    default = {
+      type = list(string)
+      default =["mongodb","redis"]
+}
 
-         mongodb = "t3.micro"
-         redis = "t3.micro"
-         rabbitmq = "t3.micro"
-         catalogue = "t3.small"
-         payment = "t3.small"
-    }
+variable "zone_id" {
+
+    type = string
+    default = "Z05454352LP47ZQWSG7WJ"
+}
+
+variable "domian_name" {
+
+    type = string
+    default = "dsecops88.online"
 }
