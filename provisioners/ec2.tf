0ea4c0500f34fd1f04642473a8ff5fd6bce4b30f
unknown <dasariravid145@gmail.com>
2026-05-04 09:40:59 +0530

    action

--- provisioners/ec2.tf
+++ provisioners/ec2.tf
@@ -7,33 +7,52 @@ resource "aws_instance" "example"{
 
    vpc_security_group_ids = [aws_security_group.allow_tls.id]
 
+   provisioner "local-exec"{
+         command = "echo '${self.public_ip}' > inventory.ini"
+    }
+
+    provisioner "local-exec"{
+         when = destroy
+         command = "echo 'Deleting the instance'"
+    }
+    provisioner "local-exec" {
+        command = "exit 1"
+    }
+
+    provisioner "local-exec"{
+         when = destroy
+         command = "echo > inventory.ini"
+    }
+    
+
+
     tags = {
 
-       Name = "terraform"
+       Name = "provisioner-demo"
           Project = "roboshop"
     }
 }
 
 
-#resource "aws_security_group" "allow_tls" {
+resource "aws_security_group" "allow_tls" {
    name        = "allow-all-roboshop"
    description = "Allow TLS inbound traffic and all outbound traffic"
 
 
      egress {
-         from_port        = 0
-         to_port          = 0
-         protocol         = "-1"
-         cidr_blocks      = ["0.0.0.0/0"]
-         ipv6_cidr_blocks = ["::/0"]
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
     }
 
     ingress {
-         from_port        = 0
-         to_port          = 0
-         protocol         = "-1"
-         cidr_blocks      = ["0.0.0.0/0"]
-         ipv6_cidr_blocks = ["::/0"]
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
     }
 
     tags= {
