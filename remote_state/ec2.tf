3256d442ef63d714d86d786caae3cbd1b2558aec
unknown <dasariravid145@gmail.com>
2026-05-04 11:04:23 +0530

    action

--- remote_state/ec2.tf
+++ remote_state/ec2.tf
@@ -4,6 +4,8 @@ resource "aws_instance" "example"{
        instance_type = "t3.micro"
        vpc_security_group_ids = [aws_security_group.allow_tls.id]
 
+    
+
     tags = {
 
         Name = "terraform-remote-state"
