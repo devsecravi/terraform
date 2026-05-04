d061699da7a9b0f29e70b45ec8c997a1aa59f407
unknown <dasariravid145@gmail.com>
2026-05-04 11:02:28 +0530

    action

--- provisioners/ec2.tf
+++ provisioners/ec2.tf
@@ -10,20 +10,49 @@ resource "aws_instance" "example"{
    provisioner "local-exec"{
          command = "echo '${self.public_ip}' > inventory.ini"
     }
+    provisioner "local-exec" {
+        command = "exit 1"
+        on_failure = continue
+    }
+
+     provisioner "local-exec" {
+        command = "echo script-2"
+    }
 
     provisioner "local-exec"{
          when = destroy
          command = "echo 'Deleting the instance'"
     }
-    provisioner "local-exec" {
-        command = "exit 1"
-    }
+
 
     provisioner "local-exec"{
          when = destroy
          command = "echo > inventory.ini"
     }
-    
+
+    connection {
+        type = "ssh"
+        user = "ec2-user"
+        password = "DevOps321"
+        host = self.public_ip
+    }
+
+    provisioner "remote-exec" {
+
+       inline = [
+             "sudo dnf install nginx -y",
+             "sudo systemctl start nginx"
+       ]
+    }
+
+provisioner "remote-exec" {
+
+       inline = [
+
+            "sudo systemctl stop nginx"
+       ]
+       when = destroy
+} 
 
 
     tags = {
